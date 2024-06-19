027e83c422236baca5274efe8d402f5ed99782ab
EXCELINDIA\Shashikanth.P <shashikanth.p@pearson.com>
2024-06-19 16:06:00 +0530

    Add index.js and package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+let person = {
+  name:"Shashikanth",
+  age:30,
+  job:"Software"
+};
+
+// console.log(person.name);
+
+let cars = ["i20","Brezza","Creta"];
+
+// console.log(cars[2]);
+
+console.log(cars.map(car=>({name:car})));
